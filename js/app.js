16c3bc0004b252be9ad6fbd336c3d0e516adc46f
andrewplan <a.plan88@gmail.com>
2016-06-28 20:46:37 -0500

    Created basic version of shopping app, will fork here to accomplish stretch goals.

--- js/app.js
+++ js/app.js
@@ -3,22 +3,22 @@ var unitPriceValue;
 var quantityValue;
 var costValue;
 
-var itemValueArray = [];
-var unitPriceValueArray = [];
-var quantityValueArray = [];
-var costValueArray = [];
-
 $(document).ready(function(){
+    $( "form" )
+        .submit(function(event) {
+            event.preventDefault();
+            var itemValue = $( ".item-value" ).val();
+            $( ".shopping-items" ).append("<li><input class=delete type='button' value='delete'><input class=checkboxes type='checkbox'>" + itemValue + "</li>").show();
+            $(".item-value").val("");
+        });
+    
+    $('ul').on('click', 'input.delete', function(event){
+        // fires when any LIs are clicked on
+        // including LIs that aren't on the page when it is initially loaded
+        $( this ).parent().remove();
+        });
 
-     $( "input.item-input" )
-        .keyup(function(event) {
-        itemValue = $( this ).val();
-        itemValueArray.push(itemValue);
-        console.log(itemValue);
-      })
-        .keyup();
-
-    $( "input.unit-price-input" )
+   /* $( "input.unit-price-input" )
         .keyup(function() {
         unitPriceValue = $( this ).val();
         unitPriceValueArray.push(unitPriceValue);
@@ -39,26 +39,18 @@ $(document).ready(function(){
          costValue = $( this ).val();
          costValueArray.push(costValue);
          console.log(costValue);
-         if ( event.keyCode == 13 ) {
-            $( ".shopping-items" ).append( "<li>" + "<span class=item-value-output>" + itemValue + "</span>" + "<span>" + unitPriceValue + "</span>" + "<span>" + quantityValue + "</span>" + "<span>" + costValue + "</span>" + "</li>" );
-             console.log(itemValue, unitPriceValue, quantityValue, costValue);
-             $("input").val("");
-            };
       })    
-         .keyup();
-    
+         .keyup(); 
+         
     $(".subtotal").append(function() {
         var total = 0;
         for (var i = 0; i < costValueArray.length; i++) {
             total += costValueArray[i] << 0;
             console.log(total);
         }
-    });
+    });*/
+    
 
-        $('ul').on('click', 'li:not(.column-names, .inputs)', function(event){
-        // fires when any LIs are clicked on
-        // including LIs that aren't on the page when it is initially loaded
-        alert("you clicked an li!");
-        });
+        
 });
 
