4830f2cc2c1df9eba7ba5090858e437732667aee
andrewplan <a.plan88@gmail.com>
2016-06-29 10:00:17 -0500

    Worked on total cost functionality.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
-var itemValue;
-var unitPriceValue;
-var quantityValue;
-var costValue;
-
 $(document).ready(function(){
+    
+    
+    
+    /*--var arr = [1,2,3,4];
+var total=0;
+for(var i in arr) { total += arr[i]; } */
+    
+    function getTotalCost () {
+                var allItemCosts; 
+                allItemCosts = $(".cost-output").text()/*document.getElementsByClassName("cost-output")*/;
+                var iter = 0;
+                var totalCost = 0;
+                for (iter = 0; iter < allItemCosts.length; ++iter) {
+                        totalCost = totalCost + allItemCosts[iter].innerHTML; 
+                        console.log (totalCost);
+                        $(".subtotal").append(totalCost);
+                    }
+                };
+    
     $( "form" )
         .submit(function(event) {
             event.preventDefault();
             var itemValue = $( ".item-value" ).val();
-            $( ".shopping-items" ).append("<li><input class=delete type='button' value='delete'><input class=checkboxes type='checkbox'>" + itemValue + "</li>").show();
-            $(".item-value").val("");
+            var quantityValue = $( ".quantity-value" ).val();
+            var unitPriceValue = $( ".unit-price-value" ).val();
+            var costValue = quantityValue * unitPriceValue;
+            $( ".shopping-list" ).append("<li class=shopping-items><input class=delete type='button' value='delete'><input class=checkboxes type='checkbox'>" + itemValue + "<span>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + costValue + "</span>" + "</li>").show();
+            $( ".item-value" ).val("");
+            $( ".quantity-value" ).val("");
+            $( ".unit-price-value" ).val("");
+            getTotalCost;
         });
     
     $('ul').on('click', 'input.delete', function(event){
@@ -17,40 +37,7 @@ $(document).ready(function(){
         // including LIs that aren't on the page when it is initially loaded
         $( this ).parent().remove();
         });
-
-   /* $( "input.unit-price-input" )
-        .keyup(function() {
-        unitPriceValue = $( this ).val();
-        unitPriceValueArray.push(unitPriceValue);
-        console.log(unitPriceValue);
-      })
-        .keyup();
-
-    $( "input.quantity-input" )
-        .keyup(function() {
-        quantityValue = $( this ).val();
-        quantityValueArray.push(quantityValue);
-        console.log(quantityValue);
-      })
-        .keyup();
-
-     $( "input.cost-input" )
-        .keyup(function() {
-         costValue = $( this ).val();
-         costValueArray.push(costValue);
-         console.log(costValue);
-      })    
-         .keyup(); 
-         
-    $(".subtotal").append(function() {
-        var total = 0;
-        for (var i = 0; i < costValueArray.length; i++) {
-            total += costValueArray[i] << 0;
-            console.log(total);
-        }
-    });*/
-    
-
+             
         
 });
 
