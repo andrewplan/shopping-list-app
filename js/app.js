ad61c34c8d2094adb49160a84a5af2bf714ff45b
andrewplan <a.plan88@gmail.com>
2016-06-30 12:56:42 -0500

    Worked on budgeting functionality some more, will get Darcy's feedback.

--- js/app.js
+++ js/app.js
@@ -5,45 +5,53 @@ $(document).ready(function(){
         var iter = 0;
         var totalCost = 0;
         var budgetLeft = 0;
-        var budget = $( "#POST-budget" ).val();
+        var budget;
 
         for (iter = 0; iter < allItemCosts.length; ++iter) {
                 totalCost = totalCost + Number(allItemCosts[iter].innerHTML);
-                budgetLeft = Number.parseFloat(budget - totalCost);
+                if ($( "#POST-budget" ).val() == "") {
+                    budget = 0
+                    }
+                else {
+                    budget = parseFloat($( "#POST-budget" ).val());
+                    }
+                budgetLeft = Number(budget - totalCost);
                 console.log (totalCost);
-                $("#total").text(totalCost);
-                $("#money-left-output").text(budgetLeft);
+                $("#total").text(totalCost.toFixed(2));
+                $("#money-left-output").text(budgetLeft.toFixed(2));
         }
     };
     
-    $( "#item-input" )
-        .submit(function(event) {
+    $("#budget-summary").submit(function(event){
+        event.preventDefault();
+        getTotalCost();
+    });
+    
+    $("#item-input").submit(function(event) {
             event.preventDefault();
-            var itemValue = parseFloat($( "#POST-item-value" ).val());
-            var quantityValue = parseFloat($( "#POST-quantity-value" ).val());
-            var unitPriceValue = parseFloat($( "#POST-unit-price-value" ).val());
-            if (itemValue = "", quantityValue = "", unitPriceValue = "") {
-                alert (You must fill out all fields!);
-            }
+            var itemValue = $( "#POST-item-value" ).val();
+            var quantityValue = $( "#POST-quantity-value" ).val();
+            var unitPriceValue = $( "#POST-unit-price-value" ).val();
             var costValue = quantityValue * unitPriceValue;
-            $( ".shopping-list" ).append("<li class=shopping-items><input class=checkboxes type='checkbox'>" + "<span class=item-name-output>" + itemValue + "</span>" + "<span class=quantity-output>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + costValue + "</span>" + "<input class=delete type='button' value='delete'></li>").show();
-            $( "#POST-item-value" ).val("");
-            $( "#POST-quantity-value" ).val("");
-            $( "#POST-unit-price-value" ).val("");
-            getTotalCost();
+        
+            if(quantityValue.match(/^(?:\d*\.\d{1,2}|\d+)$/) && unitPriceValue.match(/^(?:\d*\.\d{1,2}|\d+)$/)) {
+                $( ".shopping-list" ).append("<li class=shopping-items><input class=checkboxes type='checkbox'>" + "<span class=item-name-output>" + itemValue + "</span>" + "<span class=quantity-output>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + Math.round(costValue).toFixed(2) + "</span>" + "<input class=delete type='button' value='delete'></li>").show();
+                $( "#POST-item-value" ).val("");
+                $( "#POST-quantity-value" ).val("");
+                $( "#POST-unit-price-value" ).val("");
+                getTotalCost();
+            }
+            else {
+                alert("You must enter a valid quantity and unit price!");
+            }
     });
     
     $('ul').on('click', 'input.delete', function(event){
-        // fires when any LIs are clicked on
-        // including LIs that aren't on the page when it is initially loaded
         $( this ).parent().remove();
         getTotalCost();
     });
              
-    $( "#budget-summary" ).submit(function(event){
-        event.preventDefault();
-        getTotalCost();
-    });
+   
         
     $(document).on('change', 'input:checkbox', function(){
         var input = $(this).siblings('span');
