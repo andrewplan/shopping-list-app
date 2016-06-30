88afe6046ebb8e3f72172292c6694d82e8109b7a
andrewplan <a.plan88@gmail.com>
2016-06-29 23:40:10 -0500

    Started revising code as per Darcy's feedback from mentor call today.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,54 @@
 $(document).ready(function(){
     
-    
-    
-    /*--var arr = [1,2,3,4];
-var total=0;
-for(var i in arr) { total += arr[i]; } */
-    
     function getTotalCost () {
-                var allItemCosts; 
-                allItemCosts = $(".cost-output").text()/*document.getElementsByClassName("cost-output")*/;
-                var iter = 0;
-                var totalCost = 0;
-                for (iter = 0; iter < allItemCosts.length; ++iter) {
-                        totalCost = totalCost + allItemCosts[iter].innerHTML; 
-                        console.log (totalCost);
-                        $(".subtotal").append(totalCost);
-                    }
-                };
+        var allItemCosts = document.getElementsByClassName("cost-output");
+        var iter = 0;
+        var totalCost = 0;
+        var budgetLeft = 0;
+        var budget = $( "#POST-budget" ).val();
+
+        for (iter = 0; iter < allItemCosts.length; ++iter) {
+                totalCost = totalCost + Number(allItemCosts[iter].innerHTML);
+                budgetLeft = Number(budget - totalCost);
+                console.log (totalCost);
+                $("#total").text(totalCost);
+                $("#money-left-output").text(budgetLeft);
+        }
+    };
     
-    $( "form" )
+    $( "#item-input" )
         .submit(function(event) {
             event.preventDefault();
-            var itemValue = $( ".item-value" ).val();
-            var quantityValue = $( ".quantity-value" ).val();
-            var unitPriceValue = $( ".unit-price-value" ).val();
+            var itemValue = $( "#POST-item-value" ).val();
+            var quantityValue = $( "#POST-quantity-value" ).val();
+            var unitPriceValue = $( "#POST-unit-price-value" ).val();
             var costValue = quantityValue * unitPriceValue;
-            $( ".shopping-list" ).append("<li class=shopping-items><input class=delete type='button' value='delete'><input class=checkboxes type='checkbox'>" + itemValue + "<span>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + costValue + "</span>" + "</li>").show();
-            $( ".item-value" ).val("");
-            $( ".quantity-value" ).val("");
-            $( ".unit-price-value" ).val("");
-            getTotalCost;
-        });
+            $( ".shopping-list" ).append("<li class=shopping-items><input class=checkboxes type='checkbox'>" + "<span class=item-name-output>" + itemValue + "</span>" + "<span class=quantity-output>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + costValue + "</span>" + "<input class=delete type='button' value='delete'></li>").show();
+            $( "#POST-item-value" ).val("");
+            $( "#POST-quantity-value" ).val("");
+            $( "#POST-unit-price-value" ).val("");
+            getTotalCost();
+    });
     
     $('ul').on('click', 'input.delete', function(event){
         // fires when any LIs are clicked on
         // including LIs that aren't on the page when it is initially loaded
         $( this ).parent().remove();
-        });
+        getTotalCost();
+    });
              
+    $( "#budget-summary" ).submit(function(event){
+        event.preventDefault();
+        getTotalCost();
+    });
         
+    $(document).on('change', 'input:checkbox', function(){
+        var input = $(this).siblings('span');
+        if (this.checked) {
+            $(input).css('textDecoration', 'line-through');
+        } else {
+            $(input).css('textDecoration', 'none');
+        }
+    });
 });
 
