ae528c590518fd7da4636d9f430ebd7ec961de1c
andrewplan <a.plan88@gmail.com>
2016-07-02 22:35:13 -0500

    Fixed bug occuring when all items deleted, refined formatting of elements, made it so that item-quantity-cost column titles would show/hide depending on user actions.

--- js/app.js
+++ js/app.js
@@ -1,54 +1,71 @@
-$(document).ready(function(){
-    
-    function getTotalCost () {
+$(document).ready(function () {
+    $(".column-names").hide();
+    function getTotalCost() {
         var allItemCosts = document.getElementsByClassName("cost-output");
         var iter = 0;
         var totalCost = 0;
         var budgetLeft = 0;
         var budget;
-
-        for (iter = 0; iter < allItemCosts.length; ++iter) {
-                totalCost = totalCost + Number(allItemCosts[iter].innerHTML);
-                if ($( "#POST-budget" ).val() == "") {
-                    budget = 0
-                    }
-                else {
-                    budget = parseFloat($( "#POST-budget" ).val());
-                    }
-                budgetLeft = Number(budget - totalCost);
-                console.log (totalCost);
-                $("#total").text(totalCost.toFixed(2));
-                $("#money-left-output").text(budgetLeft.toFixed(2));
+        
+        if ($( "#POST-budget" ).val() == "") {
+            budget = 0
         }
-    };
+        else {
+            budget = parseFloat($( "#POST-budget" ).val());
+        }
+        
+        if (allItemCosts.length === 0) {
+            console.log (totalCost);
+            $("#total").text("0.00");
+            $("#money-left-output").text(budget.toFixed(2));
+        }
+        else {
+            for (iter = 0; iter < allItemCosts.length; ++iter) {
+                    totalCost = totalCost + Number(allItemCosts[iter].innerHTML);
+                    budgetLeft = Number(budget - totalCost);
+                    console.log (totalCost);
+                    $("#total").text(totalCost.toFixed(2));
+                    $("#money-left-output").text(budgetLeft.toFixed(2));
+            }
+        }
+    }
     
-    $("#budget-summary").submit(function(event){
+    $("#budget-summary").submit(function() {
         event.preventDefault();
         getTotalCost();
     });
     
-    $("#item-input").submit(function(event) {
-            event.preventDefault();
-            var itemValue = $( "#POST-item-value" ).val();
-            var quantityValue = $( "#POST-quantity-value" ).val();
-            var unitPriceValue = $( "#POST-unit-price-value" ).val();
-            var costValue = quantityValue * unitPriceValue;
+    $("#item-input").submit(function() {
+        event.preventDefault();
+        var itemValue = $( "#POST-item-value" ).val();
+        var quantityValue = $( "#POST-quantity-value" ).val();
+        var unitPriceValue = $( "#POST-unit-price-value" ).val();
+        var costValue = quantityValue * unitPriceValue;
         
-            if(quantityValue.match(/^(?:\d*\.\d{1,2}|\d+)$/) && unitPriceValue.match(/^(?:\d*\.\d{1,2}|\d+)$/)) {
-                $( ".shopping-list" ).append("<li class=shopping-items><input class=checkboxes type='checkbox'>" + "<span class=item-name-output>" + itemValue + "</span>" + "<span class=quantity-output>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + Math.round(costValue).toFixed(2) + "</span>" + "<input class=delete type='button' value='delete'></li>").show();
-                $( "#POST-item-value" ).val("");
-                $( "#POST-quantity-value" ).val("");
-                $( "#POST-unit-price-value" ).val("");
-                getTotalCost();
-            }
-            else {
-                alert("You must enter a valid quantity and unit price!");
-            }
+        $(".column-names").show();
+        if(quantityValue.match(/^(?:\d*\.\d{1,2}|\d+)$/) && unitPriceValue.match(/^(?:\d*\.\d{1,2}|\d+)$/)) {
+            $( ".shopping-list" ).append("<li class=shopping-items><input class=checkboxes type='checkbox'>" + "<span class=item-name-output>" + itemValue + "</span>" + "<span class=quantity-output>" + "(" + quantityValue + ")" + "</span>" + "<span class=cost-output>" + Math.round(costValue).toFixed(2) + "</span>" + "<input class=delete type='button' value='delete'></li>").show();
+            $( "#POST-item-value" ).val("");
+            $( "#POST-quantity-value" ).val("");
+            $( "#POST-unit-price-value" ).val("");
+            getTotalCost();
+        }
+        else {
+            alert("You must enter a valid quantity and unit price!");
+        }
     });
     
     $('ul').on('click', 'input.delete', function(event){
+        if( $(".shopping-list").children().length === 1 ) {
+            $(".column-names").hide();
+        }
+        else {
+            $(".column-names").show();
+        }
+        
         $( this ).parent().remove();
-        getTotalCost();
+        getTotalCost();       
+               
     });
              
     $(document).on('change', 'input:checkbox', function(){
